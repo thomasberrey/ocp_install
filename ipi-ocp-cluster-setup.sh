5faf25a0866f9b744da299ec3c43e2b55a69ccd6
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-05-25 23:07:04 +0000

    Updated ipi-ocp-cluster-setup.sh

--- ipi-ocp-cluster-setup.sh
+++ ipi-ocp-cluster-setup.sh
@@ -3,7 +3,7 @@
 CLUSTER_NAME=$1
 CLUSTER_ADMIN_USER=kubeadmin
 CLUSTER_ADMIN_PASSWORD=`cat $CLUSTER_NAME/auth/kubeadmin-password`
-CLUSTER_API_SERVER=`cat $CLUSTER_NAME/auth/kubeconfig | grep server: | awk '{print $2}'`
+CLUSTER_API_SERVER=`cat $CLUSTER_NAME/auth/kubeconfig | grep server: | awk '{print $2}' | uniq`
 CLUSTER_CONSOLE_SERVER=`grep "Access the OpenShift web-console here:" $CLUSTER_NAME/.openshift_install.log | sed 's/^.*Access the OpenShift web-console here: //g' | sed 's/\"//g'`
 
 echo "CLUSTER_NAME: $CLUSTER_NAME"
@@ -15,6 +15,8 @@ echo "CLUSTER_CONSOLE_SERVER: $CLUSTER_CONSOLE_SERVER"
 oc login -u $CLUSTER_ADMIN_USER -p $CLUSTER_ADMIN_PASSWORD $CLUSTER_API_SERVER
 
 # Get Argo CD Initial Password
-ARGOCD_INITIAL_ADMIN_PASSWORD=`oc -n openshift-operators get secret argocd-cluster -o jsonpath="{.data.admin\.password}" | base64 -d`
+ARGOCD_INITIAL_ADMIN_PASSWORD=`oc get secret openshift-gitops-cluster -n openshift-gitops -ojsonpath='{.data.admin\.password}' | base64 -d`
+ARGOCD_ROUTE=`oc get route openshift-gitops-server -ojsonpath=’{.spec.host}’ -n openshift-gitops`
 
 echo "ARGOCD_INITIAL_ADMIN_PASSWORD: $ARGOCD_INITIAL_ADMIN_PASSWORD"
+echo "ARGOCD_ROUTE: $ARGOCD_ROUTE"
