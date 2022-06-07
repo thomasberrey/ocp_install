cadbe80a4c2f9a15765f354c3be227e82d7614f9
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-06-07 18:16:55 +0000

    Added Service Mesh Control Plane installation

--- openshift_service_mesh_install.sh
+++ openshift_service_mesh_install.sh
@@ -2,3 +2,7 @@
 
 # Install Elasticsearch, Jaeger, Kiali and Service Mesh Operators
 oc apply -f service-mesh-operator-resources.yml
+
+# Create Service Mesh Control Plane
+oc new-project istio-system
+oc apply -f service-mesh-control-plane.yml
