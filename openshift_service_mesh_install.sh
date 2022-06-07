87d037364989bd6bd794bdafa7ae1ff4c55d14de
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-06-07 18:33:45 +0000

    Added Service Mesh Member Roll installation

--- openshift_service_mesh_install.sh
+++ openshift_service_mesh_install.sh
@@ -6,3 +6,6 @@ oc apply -f service-mesh-operator-resources.yml
 # Create Service Mesh Control Plane
 oc new-project istio-system
 oc apply -f service-mesh-control-plane.yml
+
+# Create Member Roll
+oc apply -f service-mesh-member-roll.yml
