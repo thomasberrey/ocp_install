60223f5fc45890787325a0bcec76b783aea99ff5
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-06-08 15:12:43 +0000

    Added GitOps Operator installation

--- openshift_gitops_install.sh
+++ openshift_gitops_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Get Argo CD Initial Password
-ARGOCD_INITIAL_ADMIN_PASSWORD=`oc get secret openshift-gitops-cluster -n openshift-gitops -ojsonpath='{.data.admin\.password}' | base64 -d`
-ARGOCD_ROUTE=`oc get route openshift-gitops-server -ojsonpath=’{.spec.host}’ -n openshift-gitops`
-
-echo "ARGOCD_INITIAL_ADMIN_PASSWORD: $ARGOCD_INITIAL_ADMIN_PASSWORD"
-echo "ARGOCD_ROUTE: $ARGOCD_ROUTE"
+# Install GitOps Operator
+oc apply -f gitops-operator-resources.yml
