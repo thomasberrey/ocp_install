16b35a46f46a5db1c449e0a7ebc0d9b5f3932e7a
Tom Berrey <tberrey@redhat.com>
2022-06-11 21:44:42 -0500

    Updated oc-mirror-pull.sh

--- oc-mirror-pull.sh
+++ oc-mirror-pull.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Description of oc-mirror at https://cloud.redhat.com/blog/how-oc-mirror-will-help-you-reduce-container-management-complexity
+# Download oc-mirror at https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/
+
 ./oc-mirror --config imageset-config.yml file://output-dir
