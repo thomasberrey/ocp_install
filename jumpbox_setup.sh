cd63ad74b1921a49aa59ede1767b39f0ddc8307e
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-07-25 15:47:30 +0000

    Added download of rosa

--- jumpbox_setup.sh
+++ jumpbox_setup.sh
@@ -6,15 +6,19 @@ cd /tmp
 
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/openshift-install-linux-$OCP_VERSION.tar.gz
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/openshift-client-linux-$OCP_VERSION.tar.gz
+wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/rosa/latest/rosa-linux.tar.gz
 
 tar -xzvf openshift-install-linux-$OCP_VERSION.tar.gz
 tar -xzvf openshift-client-linux-$OCP_VERSION.tar.gz
+tar -xzvf rosa-linux.tar.gz
 
 sudo cp openshift-install /usr/bin
 sudo cp oc /usr/bin
+sudo cp rosa /usr/bin
 
 sudo chown root:root /usr/bin/openshift-install
 sudo chown root:root /usr/bin/oc
+sudo chown root:root /usr/bin/rosa
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
@@ -22,4 +26,5 @@ chmod 700 get_helm.sh
 
 ls -lah `(which openshift-install)`
 ls -lah `(which oc)`
+ls -lah `(which rosa)`
 ls -lah `(which helm)`
