b7e6c5a9bbd71d730e06f26934359f01fa6dbb92
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-07-30 21:42:12 +0000

    Updated to change Rancher server admin password

--- rancher_install.sh
+++ rancher_install.sh
@@ -2,7 +2,7 @@
 
 # Install Rancher
 AWS_REGION=`cat ~/.aws/config | grep region | awk '{print $3}'`
-echo $AWS_REGION
+RANCHER_SERVER_ADMIN_PASSWORD=adminpassword
 
 rm -rf quickstart
 
@@ -12,6 +12,7 @@ cd quickstart/rancher/aws
 
 cp terraform.tfvars.example terraform.tfvars
 sed -i "s/aws_region = .*/aws_region = \"$AWS_REGION\"/g" terraform.tfvars
+sed -i "s/rancher_server_admin_password = .*/rancher_server_admin_password = \"$RANCHER_SERVER_ADMIN_PASSWORD\"/g" terraform.tfvars
 cat terraform.tfvars
 
 terraform init
