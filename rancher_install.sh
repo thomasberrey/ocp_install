6004d3731735ad544ea3565b912e738e864fd68b
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-08-02 15:53:23 +0000

    Updated sed commands for setting AWS configuration

--- rancher_install.sh
+++ rancher_install.sh
@@ -14,9 +14,13 @@ cd quickstart/rancher/aws
 
 cp terraform.tfvars.example terraform.tfvars
 sed -i '' "s/aws_region = .*/aws_region = \"$AWS_REGION\"/g" terraform.tfvars
+sed -i "s/aws_region = .*/aws_region = \"$AWS_REGION\"/g" terraform.tfvars
 sed -i '' "s/aws_access_key = .*/aws_access_key = \"$AWS_ACCESS_KEY_ID\"/g" terraform.tfvars
+sed -i "s/aws_access_key = .*/aws_access_key = \"$AWS_ACCESS_KEY_ID\"/g" terraform.tfvars
 sed -i '' "s/aws_secret_key = .*/aws_secret_key = \"$AWS_SECRET_ACCESS_KEY\"/g" terraform.tfvars
+sed -i "s/aws_secret_key = .*/aws_secret_key = \"$AWS_SECRET_ACCESS_KEY\"/g" terraform.tfvars
 sed -i '' "s/rancher_server_admin_password = .*/rancher_server_admin_password = \"$RANCHER_SERVER_ADMIN_PASSWORD\"/g" terraform.tfvars
+sed -i "s/rancher_server_admin_password = .*/rancher_server_admin_password = \"$RANCHER_SERVER_ADMIN_PASSWORD\"/g" terraform.tfvars
 cat terraform.tfvars
 
 terraform init
