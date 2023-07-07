7517c2eadf11f39bea0c4695a1bd96885e7e8012
thomasberrey <52395769+thomasberrey@users.noreply.github.com>
2023-07-07 14:33:38 -0600

    Update jumpbox_setup.sh

--- jumpbox_setup.sh
+++ jumpbox_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OCP_VERSION=4.10.14
+OCP_VERSION=4.13.4
 TERRAFORM_VERSION=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
 
 cd /tmp
